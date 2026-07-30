f84ecfcacd2d830f85b5a37a725b3c77018b9486
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2026-07-30 17:20:26 +0100

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        result = 0
+        memo = {}
+
+        if n < 3:
+            return n
+
+        for a in range(n):
+            if not memo:
+                memo[0] = 0
+                memo[1] = 1
+                memo[2] = 2
+            elif a == 1 or a == 2:
+                continue
+            else:
+                memo[a] = memo[a-1] + memo[a-2]
+        
+        result += (memo[n-1] + memo[n-2])
+
+        return result
